8d60476652a9bbd5fc821830d7f7ebc88f6371f0
Nitesh Sharma <nitesh.grep@gmail.com>
2019-01-22 09:00:37 +0530

    adding triplet question

--- 06-sort/02-triplet.js
+++ 06-sort/02-triplet.js
@@ -5,19 +5,54 @@ function solution(A) {
   // write your code in JavaScript (Node.js 8.9.4)
 
   const len = A.length;
+  let maxTriplet;
+
   for (let i=0 ; i<len ; i++) {
     
-    console.log("------", A[i]);
-    counter = 1;
-    while( counter <= 2 ){
+    let a1 =  A[i];
+    let a2;
+    let a3;
 
-      console.log(A[i+counter]);
-      counter++;
+    for(j=i; j<len; j++){
+
+        if(a2 !== undefined && a3 !== undefined){
+          break;
+        }
+
+        if(a2 === undefined){
+          if(A[j] > a1){
+            a2 = A[j]
+          }
+
+        }else if( a2 !== undefined && a3 === undefined){
+          if(A[j] > a2){
+            a3 = A[j]
+          }
+        }
+
+        
+        
     }
 
+    const sum = a1 * a2 * a3;
 
+
+
+    if(maxTriplet === undefined){
+      maxTriplet = sum;
+    }
+
+    if(sum > maxTriplet){
+      maxTriplet = sum;
+    }
+
+    
+
+    
   }
-
+  
+  
+  return maxTriplet;
   //loop
 
   //find next two and multipication store in the last max
@@ -26,4 +61,4 @@ function solution(A) {
 
 }
 
-console.log(solution([-3,1,2,-2,5,6]));
+console.log(solution([-2, -1, 0]));
